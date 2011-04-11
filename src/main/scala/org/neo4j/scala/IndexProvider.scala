9be4cb9ba63341cdc5cd0765f8b6438081643c03
FaKod <info@fakod.eu>
2011-04-11 20:36:39 +0200

    index creation convenience trait

---
+++ src/main/scala/org/neo4j/scala/IndexProvider.scala
@@ -0,0 +1,83 @@
+package org.neo4j.scala
+
+import org.neo4j.graphdb.Node
+import org.neo4j.graphdb.index.{Index, RelationshipIndex}
+import scala.collection.mutable.{Map => mutableMap}
+import collection.JavaConversions._
+
+/**
+ * Provides Index acces as trait if needed
+ *
+ * @author Christopher Schmidt
+ * Date: 11.04.11
+ * Time: 20:27
+ */
+
+trait IndexProvider {
+
+  /**
+   * type convenience definition
+   */
+  type IndexCustomConfig = Option[Map[String, String]]
+
+  /**
+   * required DatabaseService provided by XXXServiceProvider
+   */
+  val ds: DatabaseService
+
+  /**
+   * has to be overwritten to define Node Index and configuration
+   */
+  def NodeIndexConfig: List[(String, IndexCustomConfig)] = Nil
+
+  /**
+   * has to be overwritten to define Relation Index and configuration
+   */
+  def RelationIndexConfig: List[(String, IndexCustomConfig)] = Nil
+
+  /**
+   * private cache
+   */
+  private val nodeIndexStore = mutableMap[String, Index[Node]]()
+
+  /**
+   * private cache
+   */
+  private val relationIndexStore = mutableMap[String, RelationshipIndex]()
+
+  /**
+   * constructor creates indexes
+   */
+  for (forNode <- NodeIndexConfig) {
+    nodeIndexStore += forNode._1 ->
+      (forNode._2 match {
+        case Some(config) => getIndexManager.forNodes(forNode._1, config)
+        case _ => getIndexManager.forNodes(forNode._1)
+      })
+  }
+
+  for (forRelation <- RelationIndexConfig) {
+    relationIndexStore += forRelation._1 ->
+      (forRelation._2 match {
+        case Some(config) => getIndexManager.forRelationships(forRelation._1, config)
+        case _ => getIndexManager.forRelationships(forRelation._1)
+      })
+  }
+
+  /**
+   * returns the index manager
+   * @return IndexManager the index manager
+   */
+  def getIndexManager = ds.gds.index
+
+  /**
+   * @return Option[Index[Node]] the created index if available
+   */
+  def getNodeIndex(name: String) = nodeIndexStore.get(name)
+
+  /**
+   * @return Option[RelationshipIndex] the created index if available
+   */
+  def getRelationIndex(name: String) = relationIndexStore.get(name)
+
+}
