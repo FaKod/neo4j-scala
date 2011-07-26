635d4470f6e5b405c4c28377c84313dfc15d4973
FaKod <info@fakod.eu>
2011-07-26 06:36:19 +0200

    added some geometry creation convenience methods

--- src/main/scala/org/neo4j/scala/util/SpatialUtil.scala
+++ src/main/scala/org/neo4j/scala/util/SpatialUtil.scala
@@ -17,9 +17,30 @@ import collection.JavaConversions._
 private[scala] class AddGeometry(layer: EditableLayer) {
   val gf = layer.getGeometryFactory
 
+  /**
+   * Points
+   */
   def newPoint(coordinate: Coordinate): SpatialDatabaseRecord = layer.add(gf.createPoint(coordinate))
 
+  def newMultiPoint(points: Array[Point]) = layer.add(gf.createMultiPoint(points))
+
+  def newMultiPoint(coordinates: CoordinateArraySequence) = layer.add(gf.createMultiPoint(coordinates))
+
+  /**
+   * Polygon
+   */
   def newPolygon(shell: LinearRing, holes: Array[LinearRing] = null) = layer.add(gf.createPolygon(shell, holes))
+
+  def newMultiPolygon(polygons: Array[Polygon]) = layer.add(gf.createMultiPolygon(polygons))
+
+  /**
+   * Line String
+   */
+  def newLineString(coordinates: Array[Coordinate]) = layer.add(gf.createLineString(coordinates))
+
+  def newLineString(coordinates: CoordinateArraySequence) = layer.add(gf.createLineString(coordinates))
+
+  def newMultiLineString(lineStrings: Array[LineString]) = layer.add(gf.createMultiLineString(lineStrings))
 }
 
 
@@ -49,4 +70,15 @@ object LinRing {
 
   def apply(b: Buffer[(Double, Double)])(implicit layer: EditableLayer) =
     new LinearRing(CoordArraySequence(b), layer.getGeometryFactory)
+}
+
+/**
+ * convenience object for LineString
+ */
+object lineString {
+  def apply(cs: CoordinateSequence)(implicit layer: EditableLayer) =
+    new LineString(cs, layer.getGeometryFactory)
+
+  def apply(b: Buffer[(Double, Double)])(implicit layer: EditableLayer) =
+    new LineString(CoordArraySequence(b), layer.getGeometryFactory)
 }
