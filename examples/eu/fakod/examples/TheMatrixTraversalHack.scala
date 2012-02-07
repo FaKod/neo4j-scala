b507b7c8e0407486ce91baea0db47982fa231e1a
FaKod <info@fakod.eu>
2012-02-07 19:51:57 +0100

    added typed traverser (prototype)

--- examples/eu/fakod/examples/TheMatrixTraversalHack.scala
+++ examples/eu/fakod/examples/TheMatrixTraversalHack.scala
@@ -1,21 +1,13 @@
 package eu.fakod.examples
 
-import org.neo4j.scala.{EmbeddedGraphDatabaseServiceProvider, Neo4jWrapper}
 import sys.ShutdownHookThread
-import org.neo4j.graphdb.Traverser.Order
-import collection.JavaConversions._
-import org.neo4j.graphdb._
-import collection.mutable.Buffer
+import org.neo4j.scala.{TypedTraverser, SingletonEmbeddedGraphDatabaseServiceProvider, Neo4jWrapper}
 
 /**
  * The Matrix Example
  * http://wiki.neo4j.org/content/The_Matrix
  */
-
-case class Tp[T](dao: T, tp: TraversalPosition)
-
-
-object TheMatrix2 extends App with Neo4jWrapper with EmbeddedGraphDatabaseServiceProvider {
+object TheMatrix2 extends App with Neo4jWrapper with SingletonEmbeddedGraphDatabaseServiceProvider with TypedTraverser {
 
   ShutdownHookThread {
     shutdown(ds)
@@ -30,122 +22,6 @@ object TheMatrix2 extends App with Neo4jWrapper with EmbeddedGraphDatabaseServic
     "Agent Smith" -> "Program",
     "The Architect" -> "Whatever")
 
-  def OUT = Direction.OUTGOING.asInstanceOf[Object]
-
-  def IN = Direction.INCOMING.asInstanceOf[Object]
-
-  def BREADTH_FIRST = Order.BREADTH_FIRST
-
-
-  /**
-   *
-   */
-  implicit def nodeListToTraversal(list: List[Node]) = new {
-
-    //    def startTp2[T: Manifest](b: Buffer[Object])(stopEval: (T, TraversalPosition) => Boolean)(retEval: (T, TraversalPosition) => Boolean)(f: (List[T]) => Unit): Unit = {
-    //      def args = b.toArray
-    //
-    //      val tmp = list.par.map {
-    //        n =>
-    //          n.traverse(BREADTH_FIRST,
-    //            (tp: TraversalPosition) => tp.currentNode.toCC[T] match {
-    //              case Some(x) => stopEval(x, tp)
-    //              case None => false
-    //            },
-    //            (tp: TraversalPosition) => tp.currentNode.toCC[T] match {
-    //              case Some(x) => retEval(x, tp)
-    //              case None => false
-    //            }, args: _*).getAllNodes
-    //      }
-    //
-    //      val nodes = (for (c <- tmp; n <- c) yield n).distinct
-    //
-    //      val ergList = (for (n <- nodes; t <- n.toCC[T]) yield t).toList
-    //      f(ergList)
-    //    }
-
-    def startTp2[T: Manifest](b: Buffer[Object])(stopEval: PartialFunction[(T, TraversalPosition), Boolean])(retEval: PartialFunction[(T, TraversalPosition), Boolean])(f: (List[T]) => Unit): Unit = {
-      def args = b.toArray
-
-      val tmp = list.par.map {
-        n =>
-          n.traverse(BREADTH_FIRST,
-            (tp: TraversalPosition) => tp.currentNode.toCC[T] match {
-              case Some(x) if (stopEval.isDefinedAt(x, tp)) => stopEval(x, tp)
-              case _ => false
-            },
-            (tp: TraversalPosition) => tp.currentNode.toCC[T] match {
-              case Some(x) if (retEval.isDefinedAt(x, tp)) => retEval(x, tp)
-              case _ => false
-            }, args: _*).getAllNodes
-      }
-
-      val nodes = (for (c <- tmp; n <- c) yield n).distinct
-
-      val ergList = (for (n <- nodes; t <- n.toCC[T]) yield t).toList
-      f(ergList)
-    }
-
-    /**
-     *
-     */
-    def startTp[T: Manifest](stopEval: (Tp[T]) => Boolean)(retEval: (Tp[T]) => Boolean)(b: Buffer[Object]): List[T] = {
-      def args = b.toArray
-      val tmp = list.map {
-        n =>
-          n.traverse(BREADTH_FIRST,
-            (tp: TraversalPosition) => tp.currentNode.toCC[T] match {
-              case Some(x) => stopEval(Tp(x, tp))
-              case None => false
-            },
-            (tp: TraversalPosition) => tp.currentNode.toCC[T] match {
-              case Some(x) => retEval(Tp(x, tp))
-              case None => false
-            }, args: _*).getAllNodes
-      }
-
-      val nodes = (for (c <- tmp; n <- c) yield n).distinct
-
-      (for (n <- nodes; t <- n.toCC[T]) yield t).toList
-    }
-
-    /**
-     *
-     */
-    def start[T: Manifest](retEval: (T) => Boolean)(b: Buffer[Object]): List[T] = {
-      def args = b.toArray
-      val tmp = list.par.map {
-        n =>
-          n.traverse(BREADTH_FIRST, StopEvaluator.END_OF_GRAPH,
-            (tp: TraversalPosition) => tp.currentNode.toCC[T] match {
-              case Some(x) => retEval(x)
-              case None => false
-            }, args: _*).getAllNodes
-      }
-
-      val nodes = (for (c <- tmp; n <- c) yield n).distinct
-
-      (for (n <- nodes; t <- n.toCC[T]) yield t).toList
-    }
-  }
-
-  def -- = new {
-    def apply(s: String) = new {
-      val list = Buffer[Object]()
-
-      def --> = {
-        list.prepend(OUT)
-        list.prepend(DynamicRelationshipType.withName(s).asInstanceOf[Object])
-        list
-      }
-
-      def --(s: String) = {
-        list += DynamicRelationshipType.withName(s).asInstanceOf[Object]
-        list += OUT
-        this
-      }
-    }
-  }
 
   val nodeMap = withTx {
     implicit neo =>
@@ -162,71 +38,24 @@ object TheMatrix2 extends App with Neo4jWrapper with EmbeddedGraphDatabaseServic
 
   val startNodes = nodeMap("Neo") :: nodeMap("Morpheus") :: nodeMap("Trinity") :: Nil
 
-  val erg = startNodes.start[Matrix](_.name.length > 3)(--("CODED_BY") -- ("KNOWS") -->) sortWith (_.name < _.name)
-
-  val erg2 = startNodes.start[Matrix](_.name.length > 3)(--("KNOWS") -->).foldLeft(0)(_ + _.name.length)
-
-  val erg3 = startNodes.start[Matrix](_.name.length > 3)(--("CODED_BY") -- ("KNOWS") -->).foldLeft("")(_ + _.name)
-
-  println("Relations CODED_BY and KNOWS, sorted by name: " + erg)
-  println("Relations KNOWS, length of all names: " + erg2)
-  println("Relations CODED_BY and KNOWS, all names appended: " + erg3)
-
-
-  /**
-   * the following does not seem to be very convenient ;-) for FP newbies
-   * the question is: How do we get type safety through the stop and return evaluator?
-   * In case of the return evaluator its clear that we want to return only instances of Matrix
-   * the stop evaluator should work with all kinds of nodes - right? So this implementation is "wrong"
-   *
-   * maybe its better to define Tp like this
-   *    case class Tp(dao: AnyRef, tp: TraversalPosition)
-   * and to create the dao dynamically depending on the nodes "__CLASS__" property
-   *
-   * The stop evaluator could be written f. e. like
-   *
-   * {
-   *   case Matrix(name, _) => name.equalsIgnoreCase("Stop Here") // stop if name is "Stop Here"
-   *   case s:Something => true                                   // stop if node is of type Something
-   *   case _ => false                                            // default: don't stop
-   * }
-   */
-  val ergTp = startNodes.startTp[Matrix] {
-    case Tp(x, tp) => false
-  } {
-    case Tp(x, tp) => x.name.length > 3 && tp.depth == 2
-  }(--("CODED_BY") -- ("KNOWS") -->) sortWith (_.name < _.name)
-
-  println("Relations CODED_BY and KNOWS, sorted by name and depth == 2: " + ergTp)
-
-
-  val startWithNodes = nodeMap("Neo") :: nodeMap("Morpheus") :: nodeMap("Trinity") :: Nil
-
-
-  startWithNodes.startTp2[MatrixBase](--("KNOWS") -- ("CODED_BY") -->) {
+  val erg1 = startNodes.doTraverse[MatrixBase](follow -- "KNOWS" ->- "CODED_BY") {
     case _ => false
   } {
-    case (x: Matrix, tp) if (tp.depth > 0) => x.name.length > 2
+    case (x: Matrix, tp) if (tp.depth == 3) => x.name.length > 2
     case (x: NonMatrix, _) => false
-    case _ => false
-  } {
-    result =>
-      val erg = result.sortWith(_.name < _.name)
-      println("Relations CODED_BY and KNOWS, sorted by name and depth == 2: " + erg)
-  }
+  }.toList.sortWith(_.name < _.name)
+
+  println("Relations CODED_BY and KNOWS, sorted by name and depth == 3: " + erg1)
 
 
-  startWithNodes.startTp2[MatrixBase](--("KNOWS") -- ("CODED_BY") -->) {
-    case _ => false
+  val erg2 = nodeMap("Neo").doTraverse[MatrixBase](follow(BREADTH_FIRST) -- "KNOWS" ->- "CODED_BY" -<- "FOO") {
+    END_OF_GRAPH
   } {
-    case (x: Matrix, tp) if (tp.depth > 0) => x.name.length > 2
+    case (x: Matrix, tp) if (tp.depth == 2) => x.name.length > 2
     case (x: NonMatrix, _) => false
-    case _ => false
-  } {
-    result =>
-      val erg = result.sortWith(_.name < _.name)
-      println("Relations CODED_BY and KNOWS, sorted by name and depth == 2: " + erg)
-  }
+  }.toList.sortWith(_.name < _.name)
+
+  println("Relations CODED_BY and KNOWS, sorted by name and depth == 2: " + erg2)
 
 
 }
---
+++ src/main/scala/org/neo4j/scala/TypedTraverser.scala
@@ -0,0 +1,198 @@
+package org.neo4j.scala
+
+import org.neo4j.graphdb.Traverser.Order
+import collection.mutable.Buffer
+import org.neo4j.graphdb.{TraversalPosition, Direction, DynamicRelationshipType, Node}
+
+/**
+ * Trait for a Typed Traversal API
+ */
+trait TypedTraverser {
+  self: Neo4jWrapper =>
+
+  /**
+   * Iterator convenience. Mapps a Node Iterator to a T Iterator
+   * Nodes that can't be converted to T are returned as null
+   */
+  class TypedNodeIterator[T: Manifest](nodeIter: Iterator[Node]) extends Iterable[T] {
+    def iterator = new Iterator[T] {
+      def hasNext = nodeIter.hasNext
+
+      def next: T = nodeIter.next.toCC[T] match {
+        case Some(x) => x
+        case None => null.asInstanceOf[T]
+      }
+    }
+  }
+
+  /**
+   * the follow keyword
+   * @param order defines the Order of traversal
+   */
+  def follow(order: Order) = new RelationBuffer(order)
+
+  /**
+   * the follow keyword
+   * traversal defaults to DEPTH_FIRST
+   */
+  def follow: RelationBuffer = follow(Order.DEPTH_FIRST)
+
+  /**
+   * Container to store the Direction and Relationtype
+   * @param order defines the Order of traversal
+   */
+  final class RelationBuffer(val order: Order) {
+
+    import DynamicRelationshipType._
+    import Direction._
+
+    private val list = Buffer[Object]()
+
+    private def store(d: Direction, s: String) = {
+      list += withName(s)
+      list += d
+      this
+    }
+
+    /**
+     * Incoming direction
+     * @param s String type of relation
+     */
+    def -<-(s: String) = store(INCOMING, s)
+
+    /**
+     * Both directions
+     * @param s String type of relation
+     */
+    def --(s: String) = store(BOTH, s)
+
+    /**
+     * Outgoing direction
+     * @param s String type of relation
+     */
+    def ->-(s: String) = store(OUTGOING, s)
+
+    private[scala] def get = list.toArray
+
+    private[scala] def getOrder = order
+  }
+
+  /**
+   * END_OF_GRAPH Stop Evaluator
+   * stops at end of graph only
+   */
+  final def END_OF_GRAPH[T]: PartialFunction[(T, TraversalPosition), Boolean] = {
+    case _ => false
+  }
+
+  /**
+   * ALL Return Evaluator
+   * Returns all Nodes
+   */
+  final def ALL[T]: PartialFunction[(T, TraversalPosition), Boolean] = {
+    case _ => true
+  }
+
+  /**
+   * ALL_BUT_START_NODE Return Evaluator
+   * returns all Nodes except start node
+   */
+  final def ALL_BUT_START_NODE[T]: PartialFunction[(T, TraversalPosition), Boolean] = {
+    case (_, tp) => tp.notStartNode
+  }
+
+  /**
+   * shortens Order.BREADTH_FIRST
+   */
+  final val BREADTH_FIRST = Order.BREADTH_FIRST
+
+  /**
+   * shortens Order.DEPTH_FIRST
+   */
+  final val DEPTH_FIRST = Order.DEPTH_FIRST
+
+  /**
+   * Enhances a Node with a doTraverse method
+   * @param node start node
+   */
+  protected implicit def nodeToTraverse(node: Node) = new {
+
+    /**
+     * creates a traversal Iterable starting with Node node
+     * stopEval and RetEval are called with (Nodes of) type T.
+     * The returned Iterable can be converted to List. F. e.:
+     *
+     * <pre><code>
+     * myNode.doTraverse[MatrixBase](follow(BREADTH_FIRST) -- "KNOWS" ->- "CODED_BY" -<- "FOO") {
+     *   END_OF_GRAPH
+     * } {
+     *  case (x: Matrix, tp) if (tp.depth == 2) => x.name.length > 2
+     *  case (x: NonMatrix, _) => false
+     * }.toList.sortWith(_.name < _.name)
+     * </code></pre>
+     *
+     * @tparam T result type (nodes are converted to T)
+     * @param rb RelationBuffer to define Directions and RelationTypes
+     * @param stopEval Partial Function for stop evaluator function
+     * @param retEval Partial Function for return evaluator function
+     * @return Iterable[T] of converted nodes of type T
+     */
+    def doTraverse[T: Manifest](rb: RelationBuffer)
+                               (stopEval: PartialFunction[(T, TraversalPosition), Boolean])
+                               (retEval: PartialFunction[(T, TraversalPosition), Boolean]): Iterable[T] = {
+      val traverser = node.traverse(rb.getOrder,
+        (tp: TraversalPosition) => tp.currentNode.toCC[T] match {
+          case Some(x) if (stopEval.isDefinedAt(x, tp)) => stopEval(x, tp)
+          case _ => false
+        },
+        (tp: TraversalPosition) => tp.currentNode.toCC[T] match {
+          case Some(x) if (retEval.isDefinedAt(x, tp)) => retEval(x, tp)
+          case _ => false
+        }, rb.get: _*)
+
+      import collection.JavaConversions.asScalaIterator
+      new TypedNodeIterator[T](traverser.iterator)
+    }
+  }
+
+  /**
+   * Enhances a List of Nodes with a doTraverse method
+   * @param list list of nodes
+   */
+  implicit def nodeListToTraverse(list: List[Node]) = new {
+
+    /**
+     * creates a traversal Iterable starting traversals for every Node in the list
+     * stopEval and RetEval are called with (Nodes of) type T.
+     * The returned Iterable can be converted to List. F. e.:
+     *
+     * <pre><code>
+     * myListOfNodes.doTraverse[MatrixBase](follow(BREADTH_FIRST) -- "KNOWS" ->- "CODED_BY" -<- "FOO") {
+     *   END_OF_GRAPH
+     * } {
+     *  case (x: Matrix, tp) if (tp.depth == 2) => x.name.length > 2
+     *  case (x: NonMatrix, _) => false
+     * }.toList.sortWith(_.name < _.name)
+     * </code></pre>
+     *
+     * All Nodes are preconverted to a List[T] and distinct
+     *
+     * @tparam T result type (nodes are converted to T)
+     * @param rb RelationBuffer to define Directions and RelationTypes
+     * @param stopEval Partial Function for stop evaluator function
+     * @param retEval Partial Function for return evaluator function
+     * @return Iterable[T] of converted nodes of type T
+     */
+    def doTraverse[T: Manifest](rb: RelationBuffer)
+                               (stopEval: PartialFunction[(T, TraversalPosition), Boolean])
+                               (retEval: PartialFunction[(T, TraversalPosition), Boolean]): Iterable[T] = {
+      val nodes = list.par.map {
+        _.doTraverse[T](rb)(stopEval)(retEval).toList
+      }.flatMap(l => l).distinct
+
+      new Iterable[T] {
+        def iterator = nodes.iterator
+      }
+    }
+  }
+}
